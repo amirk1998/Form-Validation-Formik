5592b15c38be086e6fbb73b6c3459a9611c12150
amirk1998 <amirk1998.pv@gmail.com>
2023-03-04 12:13:05 +0330

    Validation on Mount has been added

--- src/Components/SingUp/SingUpForm.jsx
+++ src/Components/SingUp/SingUpForm.jsx
@@ -32,6 +32,7 @@ const validationSchema = Yup.object({
 });
 
 const SignUpForm = () => {
+  //
   const onSubmit = (values) => {
     console.log(values);
   };
@@ -41,9 +42,8 @@ const SignUpForm = () => {
     initialValues,
     onSubmit,
     validationSchema,
+    validateOnMount: true,
   });
-
-  // console.log(formik.values);
 
   return (
     <div className='h-screen w-1/2'>
@@ -171,7 +171,12 @@ const SignUpForm = () => {
 
         <button
           type='submit'
-          className='focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 '
+          disabled={!formik.isValid}
+          className={
+            formik.isValid
+              ? 'focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 '
+              : 'focus:outline-none text-white bg-gray-700 font-medium rounded-lg text-sm px-5 py-2.5 mb-2  cursor-not-allowed'
+          }
         >
           Submit
         </button>
