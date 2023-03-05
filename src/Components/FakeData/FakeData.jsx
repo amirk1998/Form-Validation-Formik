649153f3e515c731ed91183ef84f4beab0272aba
amirk1998 <amirk1998.pv@gmail.com>
2023-03-05 20:42:20 +0330

    Fake Data Comp has been improved

--- src/Components/FakeData/FakeData.jsx
+++ src/Components/FakeData/FakeData.jsx
@@ -1,22 +1,29 @@
 import userData from './FakeUserData';
 import { addNewUser } from '../../services/addNewUserService';
+import { useState } from 'react';
 const FakeData = () => {
+  const [loading, setLoading] = useState(false);
+
   const postUser = async () => {
     try {
+      setLoading(true);
       await addNewUser(userData);
       console.log('Data added');
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div>
       <button
+        disabled={loading}
         onClick={postUser}
         className='focus:outline-none text-gray-800 bg-green-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 my-2  '
       >
-        Add Fake Data
+        {loading ? 'Adding...' : 'Add Fake Data'}
       </button>
     </div>
   );
